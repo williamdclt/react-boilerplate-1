307dd8b1e1b9820301d5bdc1470fd5c0d1d58507
William Duclot <william.duclot@gmail.com>
2018-02-02 12:45:28 +0000

    Testing: use enzyme-to-json

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,3 +1,8 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-15');
+
+Enzyme.configure({ adapter: new Adapter() });
