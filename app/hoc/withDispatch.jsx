57ad9b100a9baa09169925c1175954a1f46798e9
William Duclot <william.duclot@gmail.com>
2018-04-07 18:50:11 +0100

    Add AirBnB rules to eslint

--- app/hoc/withDispatch.jsx
+++ app/hoc/withDispatch.jsx
@@ -5,21 +5,21 @@ import { connect } from 'react-redux';
 const withDispatch = (actionCreator, ...args) => WrappedComponent => {
   class Wrapper extends React.Component {
     componentDidMount() {
-      this.props.hoc_actionDispatcher(...args);
+      this.props.actionDispatcher(...args);
     }
 
     render() {
-      const { hoc_actionDispatcher, ...otherProps } = this.props;
+      const { actionDispatcher, ...otherProps } = this.props;
       return <WrappedComponent {...otherProps} />;
     }
   }
 
   Wrapper.propTypes = {
-    hoc_actionDispatcher: PropTypes.func,
+    actionDispatcher: PropTypes.func,
   };
 
   const mapDispatchToProps = {
-    hoc_actionDispatcher: actionCreator,
+    actionDispatcher: actionCreator,
   };
 
   return connect(null, mapDispatchToProps)(Wrapper);
