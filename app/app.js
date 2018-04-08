e5ea7b2ab9e5bf9a548a2f617061022bf6d8273c
William Duclot <william.duclot@gmail.com>
2018-04-08 15:20:33 +0100

    Dependencies: remove sanitize.css

--- app/app.js
+++ app/app.js
@@ -12,7 +12,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
-import 'sanitize.css/sanitize.css';
 
 // Import root app
 import App from 'components/App';
